67499c0dd3ab92feb404fae2f9c6483e700d85cb
atifbitcanny <atif.habib@bitcanny.com>
2021-04-11 16:27:10 +0530

    implmented devise and devise views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def toastr_flash_class(type)
+    case type
+    when "alert"
+      "toastr.error"
+    when "notice"
+      "toastr.success"
+    else
+      "toastr.info"
+    end
+  end
 end
